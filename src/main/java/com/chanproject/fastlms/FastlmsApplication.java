e814b5d74c7605830b2199a7a914fc304dbebd7a
Chanyang Park <mn99108@naver.com>
2023-01-12 14:09:28 +0900

    fastlms 초기화 설정

---
+++ src/main/java/com/chanproject/fastlms/FastlmsApplication.java
@@ -0,0 +1,13 @@
+package com.chanproject.fastlms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastlmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastlmsApplication.class, args);
+    }
+
+}
