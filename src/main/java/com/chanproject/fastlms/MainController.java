edae45f5d14b251d01d75010de5b8e18784b3086
Chanyang Park <mn99108@naver.com>
2023-01-12 15:45:10 +0900

    Thymeleaf를 통한 view페이지 매핑

---
+++ src/main/java/com/chanproject/fastlms/MainController.java
@@ -0,0 +1,58 @@
+package com.chanproject.fastlms;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String index(){
+
+        return "index";
+    }
+
+    // 스프링 -> MVC (view -> 템플릿엔진 화면에 내용을 출력(html))
+    // .NET -> MVC (View -> 출력)
+    // python django -> MTV(Template -> 화면출력)
+    // 백엔드과정 -> V(화면에 보여진 부분) -> 프론트엔드과정
+    // V -> HTML ==> 웹페이지가
+    // V -> json ==> API
+
+
+    // request -> web -> server
+    //response -> server -> wev
+
+    @RequestMapping("/hello")
+    public void hello(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        response.setContentType("text/html;charset-UTF-8");
+
+        PrintWriter printWriter = response.getWriter();
+
+        String msg = "<html>" +
+                "<head>" +
+                "<meta charset=\"UTF-8\">" +
+                "</head>" +
+                "<body>" +
+                "<p>hello</p> <p>fastlms website!!</p>" +
+                "<p>안녕하소</p>" +
+                "</body>" +
+                "</html>";
+
+        printWriter.write(msg);
+        printWriter.close();
+
+    }
+
+
+
+
+
+}
